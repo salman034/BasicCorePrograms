0d08e4132724672772bf751a009809591000a243
unknown <you@example.com>
2022-03-13 22:07:05 +0530

    Added: Percentage of Head and Tail

--- src/basicCorePrograms/FlipCoin.java
+++ src/basicCorePrograms/FlipCoin.java
@@ -1,7 +1,28 @@
 package basicCorePrograms;
 
+import java.util.Scanner;
+
 public class FlipCoin {
     public static void main(String[] args) {
-        System.out.println("Welcom to the Bsic Core Programs");
+        int Tail = 0, Head = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a positive number :");
+        int count = scanner.nextInt();
+        scanner.close();
+
+        for (int i = 0; i < count; i++) {
+            if (Math.random() < 0.5) {
+                Tail++;
+                System.out.println("Head");
+            }
+            else {
+                Head++;
+                System.out.println("Head");
+            }
+            int headflip = (100 * Head) / count;
+            int tailflip = (100 * Tail) / count;
+            System.out.println("Percentage of head is :" + headflip);
+            System.out.println("Percentage of tail is : " + tailflip);
+        }
     }
 }
